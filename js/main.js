ba33383220d8a0d4ab86b994c8d21fc36060d9aa
Pablo <percara.pablo@gmail.com>
2025-01-04 15:03:50 -0300

    version 1.01

--- js/main.js
+++ js/main.js
@@ -1,23 +1,34 @@
-const numbers = [0, 1, 2, 3];
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 const btnPlay = document.getElementById('play');
 const btnReload = document.getElementById('reload');
 const sonido = document.getElementById('sonido');
 const visualizer = document.getElementById('audio-visualizer');
+const inicio = document.getElementById('inicio');
+
 
 btnPlay.addEventListener('click', () => {
     if (numbers.length > 0) {
-
+        inicio.style.display = 'none';
         const randomIndex = Math.floor(Math.random() * numbers.length);
         const selectedNumber = numbers[randomIndex];
 
         sonido.src = `./res/sound/${selectedNumber}.mp3`;
         sonido.play();
 
+        btnPlay.style.pointerEvents = 'none';
+        btnPlay.style.opacity = '0.5';
+
         visualizer.style.visibility = 'visible';
         visualizer.style.animationPlayState = 'running';
 
         sonido.onended = () => {
             visualizer.style.visibility = 'hidden';
+            visualizer.style.animationPlayState = 'paused';
+
+
+            btnPlay.style.pointerEvents = 'auto';
+            btnPlay.style.opacity = '1';
+
         };
 
         numbers.splice(randomIndex, 1);
@@ -25,7 +36,9 @@ btnPlay.addEventListener('click', () => {
         if (numbers.length === 0) {
             btnPlay.style.pointerEvents = 'none';
             btnPlay.style.opacity = '0.5';
-            console.log('Todos los números han sido seleccionados.');
+            btnReload.addEventListener('click', () => {
+                location.reload();
+            });
         }
     }
 });
