5976dcae374b38e818a1b0b02c08fe86448b4d5b
Pablo <percara.pablo@gmail.com>
2025-01-04 15:25:49 -0300

    version 1.01

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-//const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
-const numbers = [0, 1];
-
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 const btnPlay = document.getElementById('play');
 const btnReload = document.getElementById('reload');
 const sonido = document.getElementById('sonido');
